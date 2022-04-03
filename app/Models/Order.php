b66a22326829fa6efd3c9fabf924e63c20edebad
Ibraheem <ibraheem.alaoor@hotmail.com>
2022-04-03 20:05:57 +0300

    fixing

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -9,11 +9,11 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Order extends Model
 {
-    use HasFactory , SoftDeletes;
+    use HasFactory;
     protected $fillable = [
         'url',
         'platform_id',
-        'choice' ,
+        'choice',
     ];
 
     public function platform()
